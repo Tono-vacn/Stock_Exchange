f55a4b17731e9aeffd65683d160d04d6f5898d3f
Tono-vacn <yuchen.jiang@duke.edu>
2024-03-28 16:46:36 -0400

    add basic_header

---
+++ basic_header.h
@@ -0,0 +1,26 @@
+#ifndef BASIC_HEADER_H
+#define BASIC_HEADER_H
+
+#include "threadpool/threadpool.h"
+
+
+#include <thread>
+#include <unistd.h>
+#include <vector>
+#include <arpa/inet.h>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <error.h>
+#include <fstream>
+#include <iostream>
+#include <mutex>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string>
+#include <sys/socket.h>
+#include <sys/stat.h>
+
+#endif
